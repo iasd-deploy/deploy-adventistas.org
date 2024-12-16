eca84046be14fdd2c654692753461e02902e156b
clecyo.freitas <clecyo.freitas@adventistas.org>
2024-12-16 19:31:07 -0300

    rollback core, incompatibilidade com tradução do theme

--- app/es/wp-content/plugins/elementor-pro/vendor/composer/installed.php
+++ app/es/wp-content/plugins/elementor-pro/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'elementor/elementor-pro',
-        'pretty_version' => '3.25.x-dev',
-        'version' => '3.25.9999999.9999999-dev',
-        'reference' => '4ddfa7c8179189517e039e844deba236eab55c12',
+        'pretty_version' => '3.26.x-dev',
+        'version' => '3.26.9999999.9999999-dev',
+        'reference' => '36d9612fe833b1e3a3c32ee8d22054693be4c18b',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'elementor/elementor-pro' => array(
-            'pretty_version' => '3.25.x-dev',
-            'version' => '3.25.9999999.9999999-dev',
-            'reference' => '4ddfa7c8179189517e039e844deba236eab55c12',
+            'pretty_version' => '3.26.x-dev',
+            'version' => '3.26.9999999.9999999-dev',
+            'reference' => '36d9612fe833b1e3a3c32ee8d22054693be4c18b',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
--- app/es/wp-content/plugins/elementor/vendor/composer/installed.php
+++ app/es/wp-content/plugins/elementor/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'elementor/elementor',
-        'pretty_version' => '3.25.x-dev',
-        'version' => '3.25.9999999.9999999-dev',
-        'reference' => '695491ed9f40d53d3b8c4e5865d7b5261e0bdc7e',
+        'pretty_version' => '3.26.x-dev',
+        'version' => '3.26.9999999.9999999-dev',
+        'reference' => '1ab21d24245dfb489afaa224e4d81c71a717ade4',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'elementor/elementor' => array(
-            'pretty_version' => '3.25.x-dev',
-            'version' => '3.25.9999999.9999999-dev',
-            'reference' => '695491ed9f40d53d3b8c4e5865d7b5261e0bdc7e',
+            'pretty_version' => '3.26.x-dev',
+            'version' => '3.26.9999999.9999999-dev',
+            'reference' => '1ab21d24245dfb489afaa224e4d81c71a717ade4',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
